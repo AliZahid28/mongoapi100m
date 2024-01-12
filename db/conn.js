24662d14f9b086ae9e58bad9d362d1e91b5e34a8
alizahid28 <alizahid900482@gmail.com>
2024-01-12 18:08:12 +0500

    env file

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/atheletes').then(() => console.log('db connected')).catch(e => console.log('db not connected'))
+mongoose.connect('mongodb://localhost:27017/' + process.env.DB_NAME).then(() => console.log('db connected')).catch(e => console.log('db not connected'))
